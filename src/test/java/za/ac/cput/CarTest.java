541aa3eb3e6d7bc9dc7c5aefa9b38b4774277b46
RLX-Wilkinson <81306123+RLX-Wilkinson@users.noreply.github.com>
2021-05-15 02:10:13 +0200

    Created a Test for Collection Interface

--- src/test/java/za/ac/cput/CarTest.java
+++ src/test/java/za/ac/cput/CarTest.java
@@ -16,6 +16,49 @@ class CarTest {
     Car porche = new Car("Porche","White") ;
     Car maserati = new Car("Maserati","Red");
 
+
+    Car lexus = new Car("Lexus","Burgundy");
+    Car bugatti = new Car("Bugatti", "Blue");
+    Car honda = new Car( "Honda", "Silver");
+
+    @Test
+    void collectTest(){
+
+        Collection<Car> collectCar = new LinkedList<>();
+        //Adding
+        collectCar.add(lexus);
+        collectCar.add(bugatti);
+        collectCar.add(honda);
+
+        System.out.println("Array After Added");
+        for (Car c1 : collectCar) {
+            System.out.printf("%s\n",c1);
+        }
+
+        assertEquals(3, collectCar.size());
+
+
+        //Removing
+        collectCar.remove(bugatti);
+
+        System.out.println("\n"+"ArrayList After Removal");
+        for (Car c1 : collectCar) {
+            System.out.printf("%s\n",c1);
+        }
+
+        assertEquals(2, collectCar.size());
+
+        //Finding
+        if (collectCar.contains(honda)) {
+
+            System.out.println("\n" + " Car was Found");
+        } else {
+            System.out.println("Car was not Not Found");
+
+        }
+        assertEquals(true,collectCar.contains(honda));
+    }
+
     @Test
     void listTest() {
 
@@ -61,6 +104,9 @@ class CarTest {
         assertEquals(true,carList.contains(ferrari));
     }
 
+public boolean add(Car e){
 
+   return true;
+}
 
 }
